b8e817550b2aefeef4ed119475a0543ed3c93a70
entom <tomasz@entom.info>
2021-10-19 18:06:14 +0200

    feat(app): define function, display more examples

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
+def myFunc(name):
+    print("Hello!" + name + "!")
+
 msg = "Hello World"
 print(msg)
 print(msg.capitalize())
 print(msg.upper())
 arr = msg.split()
-print(len(arr))
+arrLength = len(arr)
+print("Array lenth: " + str(arrLength))
+
+if 5 > 1:
+    print('5 > 1 - ok')    
+
+myFunc("Tom")
+myFunc("Carl")
+myFunc("John")
