909062a084e40015d1fe620c0c3f3ed3cfa091f1
entom <tomasz@entom.info>
2021-10-19 18:29:49 +0200

    feat(app): add examples, listing and dict/object

--- app.py
+++ app.py
@@ -15,3 +15,18 @@ if 5 > 1:
 myFunc("Tom")
 myFunc("Carl")
 myFunc("John")
+
+print(type(5.1))
+print(type(5))
+print(type("5"))
+
+listing = ["apple", "banana", "cherry"]
+print("listing size is " + str(len(listing)))
+print("listing, index 0 is: " + listing[0])
+print("listing, index 1 is: " + listing[1])
+print("listing, index 2 is: " + listing[2])
+
+user = {"name": "Tom", "age": 40}
+
+print("user name: " + user.get("name"))
+print("user age: " + str(user.get("age")))
